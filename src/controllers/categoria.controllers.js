b0527350eea321891daf620323fb96e0609d5261
unknown <jonatanvillamizar@gmail.com>
2025-04-06 23:41:12 -0500

    Connecting DB - HTTP:GET

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,7 +1,17 @@
-const getCategorias = (req, res) => {
-    res.json({"Categoria":"Electrodomesticos"})
-}
+import getConnection from "./../db/database.js";
+
+const getCategorias = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const result = await connection.query(
+      "SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias"
+    );
+    res.json(result);
+  } catch (error) {
+    console.error("ERROR 500");
+  }
+};
 
 export const methodHTTP = {
-    getCategorias : getCategorias
-}
+  getCategorias: getCategorias,
+};
